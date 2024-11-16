424f11b1832b389c9ab8feb0091e0267d62542c5
Marlon4789 <cuartasm844@gmail.com>
2024-11-16 17:59:04 -0500

    tiny-editor

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from tinymce.widgets import TinyMCE
 from django import forms
 
 class ArticleForm(forms.ModelForm):
-    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}, mce_attrs={'height': 700}))
 
     class Meta:
         model = Article
